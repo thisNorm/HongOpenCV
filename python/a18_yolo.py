162d886a916b74fa4fb535a06a6f30b8d100efeb
gyubeom <kisook2557@gmail.com>
2025-09-08 18:01:16 +0900

    설치 코드드

--- python/a18_yolo.py
+++ python/a18_yolo.py
@@ -1,3 +1,6 @@
 import cv2
 import numpy as np
 from ultralytics import YOLO
+# 설치
+# pip install ultralytics
+# pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
