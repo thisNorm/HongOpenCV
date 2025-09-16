ff479d9a739b0b69dbf7772e8c0814b4c5357e5f
gyubeom <kisook2557@gmail.com>
2025-09-16 18:37:47 +0900

    yunet

--- python/a30_yunet.py
+++ python/a30_yunet.py
@@ -28,7 +28,7 @@ backend_target_pairs = [
 parser = argparse.ArgumentParser(description='YuNet: A Fast and Accurate CNN-based Face Detector (https://github.com/ShiqiYu/libfacedetection).')
 parser.add_argument('--input', '-i', type=str,
                     help='Usage: Set input to a certain image, omit if using camera.')
-parser.add_argument('--model', '-m', type=str, default='face_detection_yunet_2023mar.onnx',
+parser.add_argument('--model', '-m', type=str, default='data/face_detection_yunet_2023mar.onnx',
                     help="Usage: Set model type, defaults to 'face_detection_yunet_2023mar.onnx'.")
 parser.add_argument('--backend_target', '-bt', type=int, default=0,
                     help='''Choose one of the backend-target pair to run this demo:
@@ -119,7 +119,7 @@ if __name__ == '__main__':
             cv.imshow(args.input, image)
             cv.waitKey(0)
     else: # Omit input to call default camera
-        deviceId = 0
+        deviceId = 4
         cap = cv.VideoCapture(deviceId)
         w = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
         h = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
